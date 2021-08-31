2e75bdfa05463c2cc29dfc255fc4aeb769b69328
Kazuhisa Wada <kwada@arcsys-sol.co.jp>
2021-08-31 16:17:57 +0900

    基本情報の編集画面で地図をクリックしてもアイコンが出ない不具合を修正/トップページの集計表で、道路土工構造物の通し番号が間違っていたのを修正

--- src/app/node/rfs_client/app/scripts/controllers/shisetsu_map.js
+++ src/app/node/rfs_client/app/scripts/controllers/shisetsu_map.js
@@ -253,18 +253,18 @@ class ShisetsuMapCtrl {
         this.feature.style.externalGraphic = `images/icon/shisetsu_mng/ts_1.gif`;
       } else if (this.data.shisetsu_kbn == 21) {
         this.feature.style.externalGraphic = `images/icon/shisetsu_mng/rh_1.gif`;
-      } else if (this.data[k].shisetsu_kbn == 24) {
-        feature.style.externalGraphic = `images/icon/shisetsu_mng/br_1.gif`;
-      } else if (this.data[k].shisetsu_kbn == 25) {
-        feature.style.externalGraphic = `images/icon/shisetsu_mng/ok_1.gif`;
-      } else if (this.data[k].shisetsu_kbn == 26) {
-        feature.style.externalGraphic = `images/icon/shisetsu_mng/kf_1.gif`;
-      } else if (this.data[k].shisetsu_kbn == 27) {
-        feature.style.externalGraphic = `images/icon/shisetsu_mng/hd_1.gif`;
-      } else if (this.data[k].shisetsu_kbn == 28) {
-        feature.style.externalGraphic = `images/icon/shisetsu_mng/gk_1.gif`;
-      } else if (this.data[k].shisetsu_kbn == 29) {
-        feature.style.externalGraphic = `images/icon/shisetsu_mng/dm_1.gif`;
+      } else if (this.data.shisetsu_kbn == 24) {
+        this.feature.style.externalGraphic = `images/icon/shisetsu_mng/br_1.gif`;
+      } else if (this.data.shisetsu_kbn == 25) {
+        this.feature.style.externalGraphic = `images/icon/shisetsu_mng/ok_1.gif`;
+      } else if (this.data.shisetsu_kbn == 26) {
+        this.feature.style.externalGraphic = `images/icon/shisetsu_mng/kf_1.gif`;
+      } else if (this.data.shisetsu_kbn == 27) {
+        this.feature.style.externalGraphic = `images/icon/shisetsu_mng/hd_1.gif`;
+      } else if (this.data.shisetsu_kbn == 28) {
+        this.feature.style.externalGraphic = `images/icon/shisetsu_mng/gk_1.gif`;
+      } else if (this.data.shisetsu_kbn == 29) {
+        this.feature.style.externalGraphic = `images/icon/shisetsu_mng/dm_1.gif`;
       }
       //      }
       this.feature.data = this.data;
